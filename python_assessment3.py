b2c3fe6f4f4f3996120dc6c652e0b4feb5993acb
SYDMAZHAR <142782345+SYDMAZHAR@users.noreply.github.com>
2023-08-29 14:05:00 +0530

    Update python_assessment3.py

--- python_assessment3.py
+++ python_assessment3.py
@@ -1,28 +1,3 @@
-def sum_of_numbers(num_list):
-    total = sum(num_list)
-    return total
-
-# Sample List
-sample_list = [8, 2, 3, 0, 7]
-
-# Calculating the sum
-result = sum_of_numbers(sample_list)
-
-# Printing the result
-print("Expected Output:", result)
-
-
-
-def reverse_string(input_str):
-    return input_str[::-1]
-
-sample_string = "1234abcd"
-reversed_string = reverse_string(sample_string)
-print("Sample String:", sample_string)
-print("Reversed String:", reversed_string)
-
-
-
 def count_case_characters(input_str):
     upper_count = 0
     lower_count = 0
